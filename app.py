02da0c7c602f00b67cb08dcd5514c9650fb06d63
yoonhs1432 <82449554+yoonhs1432@users.noreply.github.com>
2026-04-22 21:20:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,11 @@ from plotly.subplots import make_subplots
 st.set_page_config(page_title="퀀트 트레이딩 대시보드", layout="wide")
 
 X_ASSET_FIXED      = 'SPY'
-TARGET_TICKERS     = ['BTC-USD', 'BITU', 'ETH-USD', 'ETHT',
-                      'SPYU', 'TQQQ', 'SOXL', 'FNGU',
+TARGET_TICKERS     = ['SPYU', 'TQQQ', 'SOXL', 'FNGU',
                       'HIBL', 'TARK', 'QPUX', 'BNKU',
                       'GDXU', 'KORU', '005930','UPXI',
+                      '----','----',
+                      'BTC-USD', 'BITU', 'ETH-USD', 'ETHT',
                       'RKLB','RKLX','OKLO','OKLL',
                       'AVAV','AVXX','TEM','TEMT',
                       'IREN','IRE','CRCL','CRCG',
