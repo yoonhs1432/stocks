af8b6a564ba04ca7047a9720f83f5616a99ceaf8
yoonhs1432 <82449554+yoonhs1432@users.noreply.github.com>
2026-05-06 22:48:12 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ CFG = Config()
 
 X_ASSET_FIXED = 'SPY'
 TARGET_TICKERS = [
-   'TQQQ','SOXL', 'FNGU', 'HIBL', 'TARK', 'QPUX', 'BNKU', 'TECL', 'LABU', 'DFEN', 'DPST',
+   'TQQQ','SOXL', 'FNGU', 'HIBL', 'TARK', 'QPUX', 'TECL', 'LABU', 'DFEN', 'DPST',
     'GDXU', 'KORU', '005930', 'BITU', 'ETHT', 'AVXX','BTC-USD','ETH-USD','AVAV',
 ]
 TICKER_DISPLAY_NAMES = {'BTC-USD': 'BTC', 'ETH-USD': 'ETH', '005930': '삼전', '000660': '하닉'}
